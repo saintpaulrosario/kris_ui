b899ef223632563b954ba0b1bd583a16a6b8ccfc
Mory Keita <mory.keita@marielsoftwares.com>
2026-07-17 17:10:34 -0400

    test

--- lib/presentation/widget/image_list_widget.dart
+++ lib/presentation/widget/image_list_widget.dart
@@ -1,35 +1,80 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import '../../model/identifier.dart';
 import 'image_item_widget.dart';
 
-class ImageListWidget extends StatelessWidget {
+class ImageListWidget extends StatefulWidget {
   final List<Identifier> imagesIdentifiers;
 
   const ImageListWidget({super.key, required this.imagesIdentifiers});
 
   @override
+  State<ImageListWidget> createState() => _ImageListWidgetState();
+}
+
+class _ImageListWidgetState extends State<ImageListWidget> {
+  int _currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    if (imagesIdentifiers.isEmpty) {
-      return const Text("No image");
+    if (widget.imagesIdentifiers.isEmpty) {
+      return const Center(child: Text("No image"));
     }
 
-    return Card(
-      child: GridView.builder(
-        shrinkWrap: true,
-        physics: const AlwaysScrollableScrollPhysics(),
-        padding: const EdgeInsets.all(4),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 1,
-          mainAxisSpacing: 2,
-          crossAxisSpacing: 2,
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        CarouselSlider.builder(
+          itemCount: widget.imagesIdentifiers.length,
+          itemBuilder: (context, index, realIndex) {
+            return Card(
+              margin: const EdgeInsets.all(8),
+              clipBehavior: Clip.antiAlias,
+              child: InteractiveViewer(
+                minScale: 1.0,
+                maxScale: 5.0,
+                panEnabled: true,
+                boundaryMargin: const EdgeInsets.all(100),
+                child: SizedBox.expand(
+                  child: ImageItemWidget(
+                    imageIdentifier: widget.imagesIdentifiers[index],
+                  ),
+                ),
+              ),
+            );
+          },
+          options: CarouselOptions(
+            viewportFraction: 1.0,
+            enlargeCenterPage: false,
+            enableInfiniteScroll: false,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+          ),
         ),
-        itemCount: imagesIdentifiers.length,
-        itemBuilder: (context, index) {
-          return ImageItemWidget(imageIdentifier: imagesIdentifiers[index]);
-        },
-      ),
+        const SizedBox(height: 8),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(
+            widget.imagesIdentifiers.length,
+            (index) => AnimatedContainer(
+              duration: const Duration(milliseconds: 250),
+              margin: const EdgeInsets.symmetric(horizontal: 3),
+              width: _currentIndex == index ? 12 : 8,
+              height: _currentIndex == index ? 12 : 8,
+              decoration: BoxDecoration(
+                color: _currentIndex == index
+                    ? Theme.of(context).colorScheme.primary
+                    : Colors.grey,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
