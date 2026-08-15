1ee80773262f32074963f0fce72a81b1da1db2fd
Mory Keita <mory.keita@marielsoftwares.com>
2026-08-15 15:53:14 -0400

    father

--- lib/presentation/widget/text_list_wiget.dart
+++ lib/presentation/widget/text_list_wiget.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart' show Divider;
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kris/logic/base_event.dart';
+import 'package:kris/logic/base_state.dart';
+import 'package:kris/logic/word/script_bloc.dart';
 import 'package:kris/logic/word/translation_bloc.dart';
+import 'package:kris/logic/word/word_bloc.dart';
+import 'package:kris/model/content.dart';
 import 'package:kris/model/identifier.dart';
+import 'package:kris/model/payload.dart';
+import 'package:kris/model/script.dart';
+import 'package:kris/model/word.dart';
 
+import '../../model/text.dart' as w;
 import 'text_widget.dart';
 
 class TextListWidget extends StatefulWidget {
@@ -20,28 +28,64 @@ class _TextListWidgetState extends State<TextListWidget> {
   @override
   void initState() {
     super.initState();
+    Set<String> scripts = context.read<ScriptBloc>().state.selections.toSet();
     context.read<TranslationBloc>().add(
-      BaseEvent.texts(identifiers: widget.identifiers),
+      BaseEvent.texts(identifiers: widget.identifiers, scripts: scripts),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        ListView.separated(
-          physics: NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          itemCount: widget.identifiers.length,
-          separatorBuilder: (_, _) => const Divider(height: 1),
-          itemBuilder: (context, index) {
-            final identifier = widget.identifiers.elementAt(index);
-            return TextWidget(identifier: identifier);
-          },
-        ),
-      ],
+    return BlocListener<
+      ScriptBloc,
+      BaseState<Script, w.Text, Content, Payload>
+    >(
+      listenWhen: (previous, current) {
+        return previous.selections != current.selections;
+      },
+      listener: (context, state) {
+        Set<String> scripts = context
+            .read<ScriptBloc>()
+            .state
+            .selections
+            .toSet();
+        context.read<WordBloc>().add(
+          BaseEvent.texts(identifiers: widget.identifiers, scripts: scripts),
+        );
+      },
+      child:
+          BlocSelector<
+            WordBloc,
+            BaseState<Word, w.Text, Content, Payload>,
+            ({Set<bool> fetching, Set<w.Text>? texts})
+          >(
+            selector: (state) {
+              return (fetching: {}, texts: {
+                
+              });
+            },
+            builder: (context, state) {
+              if (state.texts!.isEmpty) {
+                return Text("texts empty");
+              }
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  ListView.separated(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: state.texts!.length,
+                    separatorBuilder: (_, _) => const Divider(height: 1),
+                    itemBuilder: (context, index) {
+                      final text = state.texts!.elementAt(index);
+                      return TextWidget(text: text);
+                    },
+                  ),
+                ],
+              );
+            },
+          ),
     );
   }
 }
--- lib/presentation/widget/text_widget.dart
+++ lib/presentation/widget/text_widget.dart
@@ -1,104 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:kris/logic/base_event.dart';
-import 'package:kris/logic/base_state.dart';
-import 'package:kris/logic/word/script_bloc.dart';
-import 'package:kris/logic/word/translation_bloc.dart';
-
-import 'package:kris/model/content.dart';
-import 'package:kris/model/payload.dart';
-import 'package:kris/model/translation.dart';
-import 'package:kris/model/identifier.dart';
 import 'package:kris/model/text.dart' as w;
-import 'package:kris/model/script.dart';
 import 'package:kris/presentation/widget/content_list_widget.dart';
 
 import 'script/script_widget.dart';
 
-class TextWidget extends StatefulWidget {
-  final Identifier identifier;
+class TextWidget extends StatelessWidget {
+  final w.Text text;
 
-  const TextWidget({super.key, required this.identifier});
-
-  @override
-  State<TextWidget> createState() => _TextWidgetState();
-}
-
-class _TextWidgetState extends State<TextWidget>
-    with AutomaticKeepAliveClientMixin {
-  @override
-  void initState() {
-    super.initState();
-
-    context.read<TranslationBloc>().add(
-      BaseEvent.text(identifier: widget.identifier),
-    );
-  }
+  const TextWidget({super.key, required this.text});
 
   @override
   Widget build(BuildContext context) {
-    super.build(context);
-    return BlocListener<
-      ScriptBloc,
-      BaseState<Script, w.Text, Content, Payload>
-    >(
-      listenWhen: (previous, current) =>
-          previous.selections != current.selections,
-
-      listener: (context, state) {
-        context.read<TranslationBloc>().add(
-          BaseEvent.text(identifier: widget.identifier),
-        );
-      },
-
-      child:
-          BlocSelector<
-            TranslationBloc,
-            BaseState<Translation, w.Text, Content, Payload>,
-            ({bool fetching, w.Text? text})
-          >(
-            selector: (state) {
-              return (
-                fetching: state.fetching.contains(widget.identifier.sku),
-                text: state.texts[widget.identifier.sku],
-              );
-            },
-            builder: (context, state) {
-              if (state.fetching) {
-                return const SizedBox(
-                  height: 40,
-                  child: Center(child: CircularProgressIndicator()),
-                );
-              }
-
-              if (state.text == null) {
-                return const Text("Text not found");
-              }
-              final text = state.text!;
-
-              return Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      ContentListWidget(identifiers: text.contents),
-                      Padding(
-                        padding: const EdgeInsets.all(8),
-                        child: ScriptWidget(
-                          key: ValueKey(text.script.sku),
-                          identifier: text.script,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Row(
+          children: [
+            ContentListWidget(identifiers: text.contents),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ScriptWidget(
+                key: ValueKey(text.script.sku),
+                identifier: text.script,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
-
-  @override
-  bool get wantKeepAlive => true;
 }
